a4337d73beddb94e5801140b733680537c8f86bb
Artem Chernyh <artem.chernyh@noveogroup.com>
2018-02-03 23:12:49 +0700

    add exceptions

---
+++ app/exceptions/index.js
@@ -0,0 +1,9 @@
+const BadRequestException = require('./badRequestException');
+const UnauthorizedException = require('./unauthorizedException');
+const NotFoundException = require('./notFoundException');
+
+module.exports = {
+    BadRequestException,
+    UnauthorizedException,
+    NotFoundException,
+};
---
+++ app/exceptions/unauthorizedException.js
@@ -0,0 +1,15 @@
+class UnauthorizedException extends Error {
+    constructor(msg = 'Unauthorized') {
+        super(msg);
+        this.status = 401;
+        this.message = msg;
+    }
+    toJSON() {
+        return {
+            error: this.message
+        };
+    }
+}
+
+UnauthorizedException.prototype.name = UnauthorizedException.name;
+module.exports = UnauthorizedException;
